c787fd7a5a4c65c56d821379e2aca27b4d5bc5e2
Talha Akhter <talhaakhter01@gmail.com>
2021-01-12 15:34:49 -0600

    section 6 topic 113

---
+++ screens/CategoryMealScreen.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+const CategoriesMealScreen = (props) => {
+  return (
+    <View styles={styles.screen}>
+      <Text>The Category Meal Screen!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default CategoriesMealScreen;
